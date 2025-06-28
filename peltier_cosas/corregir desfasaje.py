5e4ae19ac5bed99cd4c6e7b1edf5506be050d2e7
Samuel Lopez <samuel.f.lopez@outlook.com>
2025-06-28 13:12:54 -0300

    graficos fotos y boludeces

--- peltier_cosas/corregir desfasaje.py
+++ peltier_cosas/corregir desfasaje.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 
-i=21
+i=27
 print(i)
 df=pd.read_csv(f'procesados/video{i}_Lab_sliced.csv')
 
-df = df[77:]
+df = df[92:]
 
 df['t']=df['t']-df['t'].iloc[0]
 df['t'] = df['t'].round(2)
--- video3_ripper.py
+++ video3_ripper.py
@@ -2,14 +2,15 @@ import cv2
 names=['videos/20250528_170906-003.mp4','videos/20250528_173138.mp4','videos/20250528_173920.mp4','videos/20250528_175822.mp4',
        'videos/C1862.MP4','videos/C1871.MP4','videos/C1872.MP4','videos/C1873.MP4','videos/C1883.MP4','videos/C1888.MP4','videos/C1889.MP4',
        'videos/C1896.MP4','videos/C1897.MP4','videos/C1899.MP4','videos/C1906.MP4','videos/C1907.MP4',
-       'videos/C1910.MP4','videos/C1915.MP4','videos/C1917.MP4','videos/C1918.MP4','videos/C1923.MP4']
+       'videos/C1910.MP4','videos/C1915.MP4','videos/C1917.MP4','videos/C1918.MP4','videos/C1923.MP4',
+       'videos/C1926.MP4','videos/C1928.MP4','videos/C1929.MP4','videos/C1932.MP4','videos/C1933.MP4','videos/C1934.MP4']
 import time
 import os
 
 start=time.time()
 
 
-for i in range(21,50):
+for i in range(22,23):
     print(names[i-1])
     vidcap = cv2.VideoCapture(names[i-1])
     success, image = vidcap.read()
